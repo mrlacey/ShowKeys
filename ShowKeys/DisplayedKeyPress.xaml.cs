250ca94618eb745e88f8c149d2b631bbb5e99c1e
Matt Lacey <matt@mrlacey.co.uk>
2019-10-08 12:56:36 +0100

    Generate all visuals dynamically

--- ShowKeys/DisplayedKeyPress.xaml.cs
+++ ShowKeys/DisplayedKeyPress.xaml.cs
@@ -3,23 +3,19 @@
 // </copyright>
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Forms;
 
 namespace ShowKeys
 {
-    public partial class DisplayedKeyPress : UserControl
+    public partial class DisplayedKeyPress : System.Windows.Controls.UserControl
     {
         private static Guid lastRequest;
 
         public DisplayedKeyPress()
         {
             this.InitializeComponent();
-
-            this.HideAllKeys();
         }
 
         public async Task ShowAsync(SupportedCommand cmd)
@@ -38,182 +34,182 @@ namespace ShowKeys
                 case SupportedCommand.Cut:
                     if (options.ShowCut)
                     {
-                        await this.DisplayAsync(this.control, this.x).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Control, Keys.X }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.Copy:
                     if (options.ShowCopy)
                     {
-                        await this.DisplayAsync(this.control, this.c).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Control, Keys.C }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.Paste:
                     if (options.ShowPaste)
                     {
-                        await this.DisplayAsync(this.control, this.v).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Control, Keys.V }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.Escape:
                     if (options.ShowEscape)
                     {
-                        await this.DisplayAsync(this.escape).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Escape }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.Tab:
                     if (options.ShowTab)
                     {
-                        await this.DisplayAsync(this.tab).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Tab }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.BackTab:
                     if (options.ShowBackTab)
                     {
-                        await this.DisplayAsync(this.shift, this.tab).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Shift, Keys.Tab }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.PageDown:
                     if (options.ShowPageDown)
                     {
-                        await this.DisplayAsync(this.pageUp).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.PageDown }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.Delete:
                     if (options.ShowDelete)
                     {
-                        await this.DisplayAsync(this.delete).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Delete }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.PageUp:
                     if (options.ShowPageUp)
                     {
-                        await this.DisplayAsync(this.pageUp).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.PageUp }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.CommentSelection:
                     if (options.ShowCommentSelection)
                     {
-                        await this.DisplayAsync(this.control, this.k, this.comma, this.control2, this.c).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Control, Keys.K }, new[] { Keys.Control, Keys.C }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.UncommentSelection:
                     if (options.ShowUncommentSelection)
                     {
-                        await this.DisplayAsync(this.control, this.k, this.comma, this.control2, this.u).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Control, Keys.K }, new[] { Keys.Control, Keys.U }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.DuplicateSelection:
                     if (options.ShowDuplicateSelection)
                     {
-                        await this.DisplayAsync(this.control, this.d).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Control, Keys.D }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.FormatDocument:
                     if (options.ShowFormatDocument)
                     {
-                        await this.DisplayAsync(this.control, this.k, this.comma, this.control2, this.d).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Control, Keys.K }, new[] { Keys.Control, Keys.D }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.FormatSelection:
                     if (options.ShowFormatSelection)
                     {
-                        await this.DisplayAsync(this.control, this.k, this.comma, this.control2, this.f).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Control, Keys.K }, new[] { Keys.Control, Keys.F }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.InsertSnippet:
                     if (options.ShowInsertSnippet)
                     {
-                        await this.DisplayAsync(this.control, this.k, this.comma, this.control2, this.x).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Control, Keys.K }, new[] { Keys.Control, Keys.X }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.InvokeQuickInfo:
                     if (options.ShowInvokeQuickInfo)
                     {
-                        await this.DisplayAsync(this.control, this.k, this.comma, this.control2, this.i).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Control, Keys.K }, new[] { Keys.Control, Keys.I }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.MoveSelectedLineDown:
                     if (options.ShowMoveSelectedLineDown)
                     {
-                        await this.DisplayAsync(this.alt, this.downArrow).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Alt, Keys.Down }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.MoveSelectedLineUp:
                     if (options.ShowMoveSelectedLineUp)
                     {
-                        await this.DisplayAsync(this.alt, this.upArrow).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Alt, Keys.Up }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.Redo:
                     if (options.ShowRedo)
                     {
-                        await this.DisplayAsync(this.control, this.y).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Control, Keys.Y }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.Rename:
                     if (options.ShowRename)
                     {
-                        await this.DisplayAsync(this.control, this.r, this.comma, this.control2, this.r2).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Control, Keys.R }, new[] { Keys.Control, Keys.R }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.Save:
                     if (options.ShowSave)
                     {
-                        await this.DisplayAsync(this.control, this.s).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Control, Keys.S }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.SelectAll:
                     if (options.ShowSelectAll)
                     {
-                        await this.DisplayAsync(this.control, this.a).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Control, Keys.A }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.SurroundWith:
                     if (options.ShowSurroundWith)
                     {
-                        await this.DisplayAsync(this.control, this.k, this.comma, this.control2, this.s).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Control, Keys.K }, new[] { Keys.Control, Keys.S }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.Undo:
                     if (options.ShowUndo)
                     {
-                        await this.DisplayAsync(this.control, this.z).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Control, Keys.Z }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.ViewCode:
                     if (options.ShowViewCode)
                     {
-                        await this.DisplayAsync(this.f7).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.F7 }).ConfigureAwait(false);
                     }
 
                     break;
                 case SupportedCommand.ViewDesigner:
                     if (options.ShowViewDesigner)
                     {
-                        await this.DisplayAsync(this.shift, this.f7).ConfigureAwait(false);
+                        await this.DisplayAsync(new[] { Keys.Shift, Keys.F7 }).ConfigureAwait(false);
                     }
 
                     break;
@@ -222,23 +218,42 @@ namespace ShowKeys
             }
         }
 
-        private async Task DisplayAsync(params UIElement[] keys)
+        private async Task DisplayAsync(params Keys[][] keys)
         {
             var requestId = Guid.NewGuid();
-
             lastRequest = requestId;
 
-            this.HideAllKeys(keys);
+            var keyConverter = new KeysConverter();
 
-            foreach (var key in keys)
+            this.Container.Children.Clear();
+            this.Container.Opacity = 0;
+
+            foreach (var subgroup in keys)
             {
-                key.Visibility = Visibility.Visible;
+                if (this.Container.Children.Count > 0)
+                {
+                    this.Container.Children.Add(new CombiningControl { CombiningText = ", " });
+                }
+
+                var firstInGroup = true;
+
+                foreach (var key in subgroup)
+                {
+                    if (!firstInGroup)
+                    {
+                        this.Container.Children.Add(new CombiningControl { CombiningText = "+" });
+                    }
+
+                    this.Container.Children.Add(new KeyControl
+                    {
+                        KeyText = keyConverter.ConvertToInvariantString(key),
+                    });
+
+                    firstInGroup = false;
+                }
             }
 
-            foreach (var key in keys)
-            {
-                AnimationHelper.FadeIn(key);
-            }
+            AnimationHelper.FadeIn(this.Container);
 
             // Need to remain on UI Thread to do the fadeout animations
             await Task.Delay(1500).ConfigureAwait(true);
@@ -246,60 +261,7 @@ namespace ShowKeys
             // Don't animate out if already started showing other keys.
             if (lastRequest == requestId)
             {
-                foreach (var key in keys)
-                {
-                    // Don't bother animating out if already made invisible
-                    // because of a request to show other keys.
-                    if (key.Visibility == Visibility.Visible)
-                    {
-                        AnimationHelper.FadeOut(key);
-                    }
-                }
-            }
-        }
-
-        private IEnumerable<UIElement> AllElements()
-        {
-            yield return this.control;
-            yield return this.control2;
-            yield return this.comma;
-            yield return this.a;
-            yield return this.c;
-            yield return this.d;
-            yield return this.f;
-            yield return this.i;
-            yield return this.k;
-            yield return this.r;
-            yield return this.r2;
-            yield return this.s;
-            yield return this.u;
-            yield return this.v;
-            yield return this.x;
-            yield return this.y;
-            yield return this.z;
-            yield return this.f7;
-            yield return this.shift;
-            yield return this.tab;
-            yield return this.alt;
-            yield return this.delete;
-            yield return this.escape;
-            yield return this.pageUp;
-            yield return this.pageDown;
-            yield return this.upArrow;
-            yield return this.downArrow;
-        }
-
-        private void HideAllKeys(params UIElement[] apartFrom)
-        {
-            var apartFromList = apartFrom.ToList();
-
-            foreach (var element in this.AllElements())
-            {
-                if (!apartFromList.Contains(element))
-                {
-                    element.Opacity = 0;
-                    element.Visibility = Visibility.Collapsed;
-                }
+                AnimationHelper.FadeOut(this.Container);
             }
         }
     }
